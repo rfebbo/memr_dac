9fec7c7cfbb41767f6fdf9b8d1e3171e1bc08f44
Rocco Febbo <febbo87@gmail.com.com>
2023-12-01 21:45:38 -0500

    finished characerize analog as intro

--- read_1t1r_data.py
+++ read_1t1r_data.py
@@ -5,6 +5,7 @@ import pandas as pd
 def read_1t1r_data(dir, type):
     temps = []
     data = []
+    filenames = []
     for top, _, files in os.walk(dir):
         for f in files:
             res = re.findall(f'{type}' + r'.*_(\d+k)_', f)
@@ -15,11 +16,11 @@ def read_1t1r_data(dir, type):
                 temps.append(int(temp))
 
                 df = pd.read_excel(os.path.join(top, f), 'Sheet 1')
-
+                filenames.append(f)
                 d = {} 
                 for d_i, l in enumerate(list(df.columns)):
                     d[l] = df.to_numpy()[:,d_i]
 
                 data.append(d)
 
-    return temps, data 
+    return temps, data, filenames
